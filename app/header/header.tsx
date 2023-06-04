1f1e4bd9692a797ad57822846c59892b1516b7ce
wuttyee1910@gmail.com <wuttyee1910@gmail.com>
2023-06-04 09:24:01 +0630

    second commit

--- app/header/header.tsx
+++ app/header/header.tsx
@@ -1,38 +1,103 @@
 import Image from 'next/image'
 import styles from './header.module.scss'
-import { FaApple , FaSearch , FaShoppingBag } from 'react-icons/fa';
-import NavItem from './navItem';
-
-
+import { FaApple, FaSearch, FaShoppingBag } from 'react-icons/fa';
 export default function Header() {
   return (
-
-    <header className={styles.all } >
-      <nav className={styles.nav}>
-        <div >
-        <ul className={styles.ul_list}>
-          <li><FaApple size={18}/></li>
-          <li><a href="/Store/" data-analytics-title="Store">Store</a></li>
-          <li><a href="/Mac/" data-analytics-title="Mac">Mac</a></li>
-          <li><a href="/iPad/" data-analytics-title="iPad">iPad</a></li>
-          <li><a href="/iPhone/" data-analytics-title="iPhone">iPhone</a></li>
-          <li><a href="/Watch/" data-analytics-title="Watch">Watch</a></li>
-          <li><a href="/AirPods/" data-analytics-title="AirPods">AirPods</a></li>
-          <li><a href="/TV & Home/" data-analytics-title="TV & Home">TV & Home</a></li>
-          <li><a href="/Entertainment/" data-analytics-title="Entertainment">Entertainment</a></li>
-          <li><a href="/Accessories/" data-analytics-title="Accessories">Accessories</a></li>
-          <li><a href="/Support/" data-analytics-title="Support">Support</a></li>
-          <li><FaSearch size={18}/></li>
-          <li><FaShoppingBag size={18}/></li>
-        </ul>
-        </div>
-        <div >
-            <NavItem />
-        </div>
+    <header>
+      <div className={styles.upper_header}>
+        <a className={styles.logo_icon} href="/" data-globalnav-item-name="apple" data-analytics-title="apple home" aria-label="Apple"><span>
+        </span></a>
        
-        
-        
-      </nav>
+        <ul className={styles.top_level_menu}>
+          <li>
+            <a href="#" className={styles.icon_list}><FaApple size={18} /></a>
+          </li>
+          <li><a href="#">Store</a>
+            <ul className={styles.second_level_menu}>
+              <li><a href="#">Store </a></li>
+              <li><a href="#">Store</a></li>
+              <li><a href="#">Store</a> </li>
+              <li><a href="#">Store</a></li>
+            </ul></li>
+          <li><a href="#">iPhone</a>
+            <ul className={styles.second_level_menu}>
+              <li><a href="#">iPhone </a></li>
+              <li><a href="#">iPhone</a></li>
+              <li><a href="#">iPhone</a> </li>
+              <li><a href="#">iPhone</a></li>
+            </ul></li>
+          <li>
+            <a href="#">iPad</a>
+            <ul className={styles.second_level_menu}>
+              <li><a href="#">iPad </a></li>
+              <li><a href="#">iPad</a></li>
+              <li><a href="#">iPad</a> </li>
+              <li><a href="#">iPad</a></li>
+            </ul>
+          </li>
+          <li><a href="#">iPhone</a>
+            <ul className={styles.second_level_menu}>
+              <li><a href="#">iPhone </a></li>
+              <li><a href="#">iPhone</a></li>
+              <li><a href="#">iPhone</a> </li>
+              <li><a href="#">iPhone</a></li>
+            </ul></li>
+          <li><a href="#">Watch</a>
+            <ul className={styles.second_level_menu}>
+              <li><a href="#">Watch </a></li>
+              <li><a href="#">Watch</a></li>
+              <li><a href="#">Watch</a> </li>
+              <li><a href="#">Watch</a></li>
+            </ul></li>
+          <li><a href="#">Air Pods</a>
+            <ul className={styles.second_level_menu}>
+              <li><a href="#">Air Pods </a></li>
+              <li><a href="#">Air Pods</a></li>
+              <li><a href="#">Air Pods</a> </li>
+              <li><a href="#">Air Pods</a></li>
+            </ul></li>
+          <li><a href="#">TV & Home</a>
+            <ul className={styles.second_level_menu}>
+              <li><a href="#">TV & Home </a></li>
+              <li><a href="#">TV & Home</a></li>
+              <li><a href="#">TV & Home</a> </li>
+              <li><a href="#">TV & Home</a></li>
+            </ul></li>
+          <li><a href="#">Entertainment</a>
+            <ul className={styles.second_level_menu}>
+              <li><a href="#">Entertainment </a></li>
+              <li><a href="#">Entertainment</a></li>
+              <li><a href="#">Entertainment</a> </li>
+              <li><a href="#">Entertainment</a></li>
+            </ul></li>
+          <li><a href="#">Accessories</a>
+            <ul className={styles.second_level_menu}>
+              <li><a href="#">Accessories </a></li>
+              <li><a href="#">Accessories</a></li>
+              <li><a href="#">Accessories</a> </li>
+              <li><a href="#">Accessories</a></li>
+            </ul></li>
+          <li><a href="#">Support</a>
+            <ul className={styles.second_level_menu}>
+              <li><a href="#">Support </a></li>
+              <li><a href="#">Support</a></li>
+              <li><a href="#">Support</a> </li>
+              <li><a href="#">Support</a></li>
+            </ul></li>
+            <li>
+            <a href="#" className={styles.icon_list}><FaSearch size={18} /></a>
+          </li>
+          <li>
+            <a href="#" className={styles.icon_list}><FaShoppingBag size={18} /></a>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.lower_header}>
+        <a>Apple TV+</a>
+        <a data-analytics-title="stream now" >Stream now
+          <span ></span>
+        </a>
+      </div>
     </header>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,65 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-
+import homeImg from './images/home.png';
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        {/* <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p> */}
+      <div className={styles.image_container}>
+        <Image src={homeImg}  className={styles.image}
+        width={500}
+        height={500} alt='image'/>
+      </div>
+      <section>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <div>
+            <div>
+              <div>
+                <h3><span></span></h3>
+                <p></p>
+                <p>The&nbsp;Apple&nbsp;Music Student <br />Plan comes with <br />Apple&nbsp;TV+ for&nbsp;free.<sup><a href="#footnote-4" aria-label="Footnote 4">4</a></sup></p>
+                <a href="https://music.apple.com/deeplink?app=music&amp;p=subscribe-student?itscg=10000&amp;itsct=mus-0-tv_ovp-try_mus-apl-200310" aria-label="Try apple music free" data-analytics-title="try apple music free" data-analytics-exit-link="" data-rid-relay="{&quot;286&quot;:&quot;itsct&quot;}">Try Apple Music free</a>
+              </div>
+              <div >
+                <figure>
+                  <img src="https://www.apple.com/v/apple-tv-plus/ag/images/overview/student_plan__bm7yp0v4tf6u_medium_2x.jpg" alt="Image description" />
+                  <figcaption>Caption for the image</figcaption>
+                </figure>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div data-analytics-section-engagement="name:apple tv 4k">
+              <figure>
+              <img src="https://www.apple.com/v/apple-tv-plus/ag/images/overview/apple_tv_4k_remote__igmcbznr1w2u_medium_2x.jpg" alt="Image description" />
+                  <figcaption>Caption for the image</figcaption>
+              </figure>
+              <div>
+                <h3><span>Apple&nbsp;TV&nbsp;4K</span></h3>
+                <p>The Apple experience.<br />Cinematic in every&nbsp;sense.</p>
+                <div>
+                  <ul>
+                    <li>
+                      <a href="/us/shop/goto/buy_tv/apple_tv_4k" data-analytics-title="buy - apple tv 4k" aria-label="Buy Apple TV 4K" ><span >Buy</span></a> </li>
+                    <li>
+                      <a href="/apple-tv-4k/" data-analytics-title="learn more about apple tv 4k" aria-label="Learn more about Apple TV 4K" data-rid-store="{&quot;289&quot;:&quot;atv-0-tvapp_ovp-tle_lrn-apl-avl-191111&quot;}"><span >Learn more</span><span ></span></a> </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div data-analytics-section-engagement="name:airplay">
+              <figure >
+              <img src=" https://www.apple.com/v/apple-tv-plus/ag/images/overview/air_play__dyuvvwnd5riq_medium_2x.jpg" alt="Image description" />
+                  <figcaption>Caption for the image</figcaption>
+              </figure>
+              <div >
+                <h3 >AirPlay</h3>
+                <p >Bring Apple&nbsp;TV+ to <br />a screen near you.</p>
+                <a href="/airplay/" data-analytics-title="learn more about airplay" aria-label="learn more about airplay"><span >Learn more</span><span ></span></a> </div>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
     </main>
+ 
   )
 }
